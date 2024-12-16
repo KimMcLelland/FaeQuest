f407133d5edf78f413cd911421118618766f1b35
Kim <faeriekim3@gmail.com>
2024-12-16 13:25:00 +0000

    orc spawning

--- Hills.gd
+++ Hills.gd
@@ -1,13 +1,28 @@
 extends ColorRect
 
-
+var rng = RandomNumberGenerator.new()
+var Orc = preload("res://Orc.tscn")
+var Wolf = preload("res://Wolf.tscn")
 
 
 
 func _ready():
-	pass 
+	rng.randomize()
+	orc_spawn()
+	orc_spawn()
+	orc_spawn()
+	orc_spawn()
+	orc_spawn()
 
 
 
 #func _process(delta):
 #	pass
+
+
+func orc_spawn():
+	var spawn = Orc.instance()
+	add_child(spawn)
+	spawn.position.x = rng.randi_range (32, 2016)
+	spawn.position.y = rng.randi_range (32, 1768)
+
