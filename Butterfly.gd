8df76c5489dd1b5d43c6fece285cd57f5c324d2f
Kim <faeriekim3@gmail.com>
2025-01-04 15:20:06 +0000

    Butterfly movement

--- Butterfly.gd
+++ Butterfly.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 var rng = RandomNumberGenerator.new()
-
+var direction
+var speed
 
 
 func _ready():
@@ -13,8 +14,34 @@ func _ready():
 		$AnimatedSprite.animation = "blue"
 	if variety == 3:
 		$AnimatedSprite.animation = "yellow"
+	direction = rng.randi_range(1, 4)
+	speed = 100
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2()
+	if direction == 1:
+		velocity.x += 1
+	if direction == 2:
+		velocity.y += 1
+	if direction == 3:
+		velocity.x -= 1
+	if direction == 4:
+		velocity.y -= 1
+		
+	if position.x > 2016:
+		direction = 3
+	if position.x < 32:
+		direction = 1
+	if position.y > 1768:
+		direction = 4
+	if position.y < 32:
+		direction = 2
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+
+
+func _on_Timer_timeout():
+	direction = rng.randi_range(1, 4)
