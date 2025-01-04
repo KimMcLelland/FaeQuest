8c5b2c555729b3fe036d547f750f2175f2dad3f9
Kim <faeriekim3@gmail.com>
2025-01-04 15:04:55 +0000

    cow and horse movement

---
+++ cow.gd
@@ -0,0 +1,61 @@
+extends Area2D
+
+var rng = RandomNumberGenerator.new()
+var direction
+var speed
+
+
+func _ready():
+	speed = 100
+	$AnimatedSprite.animation = "down"
+	rng.randomize()
+	direction = rng.randi_range (1, 8)
+
+
+func _process(delta):
+	var velocity = Vector2()
+	if direction == 1:
+		velocity.x += 1
+		$AnimatedSprite.animation = "right"
+	if direction == 2:
+		velocity.x += 1
+		velocity.y += 1
+		$AnimatedSprite.animation = "right"
+	if direction == 3:
+		velocity.y += 1
+		
+	if direction == 4:
+		velocity.x -= 1
+		velocity.y += 1
+		$AnimatedSprite.animation = "left"
+	if direction == 5:
+		velocity.x -= 1
+		$AnimatedSprite.animation = "left"
+	if direction == 6:
+		velocity.x -= 1
+		velocity.y -= 1
+		$AnimatedSprite.animation = "left"
+	if direction == 7:
+		velocity.y -= 1
+		
+	if direction == 8:
+		velocity.x += 1
+		velocity.y -= 1
+		$AnimatedSprite.animation = "right"
+		
+	if position.x > 2016:
+		direction = rng.randi_range (4, 6)
+	if position.x < 32:
+		var result = rng.randi_range (1, 3)
+		if result == 1 or result == 2:
+			direction = result
+		else:
+			direction = 8
+	if position.y > 1768:
+		direction = rng.randi_range (6, 8)
+	if position.y < 32:
+		direction = rng.randi_range (2, 4)
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
