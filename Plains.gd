fd51a24875dfa44ebcf71e9381420aae56e3a69a
Kim <faeriekim3@gmail.com>
2025-01-04 14:47:29 +0000

    multiple spawning for animals and plants in plains

--- Plains.gd
+++ Plains.gd
@@ -10,9 +10,23 @@ var Sparrow = preload("res://sparrow.tscn")
 func _ready():
 	rng.randomize()
 	cabbage_spawn()
+	cabbage_spawn()
+	cabbage_spawn()
+	cow_spawn()
+	cow_spawn()
 	cow_spawn()
 	horse_spawn()
+	horse_spawn()
+	horse_spawn()
 	butterfly_spawn()
+	butterfly_spawn()
+	butterfly_spawn()
+	butterfly_spawn()
+	butterfly_spawn()
+	bird_spawn()
+	bird_spawn()
+	bird_spawn()
+	bird_spawn()
 	bird_spawn()
 
 
@@ -21,10 +35,18 @@ func _ready():
 #	pass
 
 func cabbage_spawn():
-	var spawn = Cabbage.instance()
-	add_child(spawn)
-	spawn.position.x = rng.randi_range (32, 2016)
-	spawn.position.y = rng.randi_range (32, 1768)
+	var spawn1 = Cabbage.instance()
+	var spawn2 = Cabbage.instance()
+	var spawn3 = Cabbage.instance()
+	add_child(spawn1)
+	spawn1.position.x = rng.randi_range (32, 2016)
+	spawn1.position.y = rng.randi_range (32, 1768)
+	add_child(spawn2)
+	spawn2.position.x = rng.randi_range (spawn1.position.x - 64, spawn1.position.x + 64)
+	spawn2.position.y = rng.randi_range (spawn1.position.y - 64, spawn1.position.y + 64)
+	add_child(spawn3)
+	spawn3.position.x = rng.randi_range (spawn1.position.x - 64, spawn1.position.x + 64)
+	spawn3.position.y = rng.randi_range (spawn1.position.y - 64, spawn1.position.y + 64)
 
 func cow_spawn():
 	var spawn = Cow.instance()
