3abb9db9931a40984b1381b30d8a75d6b9b0bf73
Kim <faeriekim3@gmail.com>
2025-01-17 16:04:41 +0000

    woodpeckers

--- Pinetree.gd
+++ Pinetree.gd
@@ -2,7 +2,9 @@ extends StaticBody2D
 
 var rng = RandomNumberGenerator.new()
 var Cherry = preload("res://Cherry.tscn")
+var woodpecker = preload("res://Woodpecker.tscn")
 var fruit
+var bird
 
 func _ready():
 	rng.randomize()
@@ -12,6 +14,9 @@ func _ready():
 		var further_fruit = rng.randi_range (1, 3)
 		if further_fruit == 3:
 			cherry_spawn()
+	bird = rng.randi_range (1, 3)
+	if bird == 3:
+		woodpecker_spawn()
 
 
 #func _process(delta):
@@ -26,3 +31,15 @@ func cherry_spawn():
 	if spawn_position == 2:
 		spawn.position.x = -48
 	spawn.position.y = 0
+	
+func woodpecker_spawn():
+	var spawn = woodpecker.instance()
+	var spawn_position = rng.randi_range (1, 2)
+	add_child(spawn)
+	if spawn_position == 1:
+		spawn.position.x = 24
+		spawn.animation = "left"
+	if spawn_position == 2:
+		spawn.position.x = -24
+		spawn.animation = "right"
+	spawn.position.y = 32
