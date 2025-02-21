e5e320afce2bd2e7fdcc0712bfc56347797ccd3b
Kim <faeriekim3@gmail.com>
2025-02-21 14:28:55 +0000

    trees and moon shrine

--- oaktree.gd
+++ oaktree.gd
@@ -1,28 +1,10 @@
 extends StaticBody2D
 
-var rng = RandomNumberGenerator.new()
-var Apple = preload("res://apple.tscn")
-var fruit
-
 func _ready():
-	rng.randomize()
-	fruit = rng.randi_range (1, 3)
-	if fruit == 3:
-		apple_spawn()
-		var further_fruit = rng.randi_range (1, 3)
-		if further_fruit == 3:
-			apple_spawn()
+	pass
 
 
 #func _process(delta):
 #	pass
 
-func apple_spawn():
-	var spawn = Apple.instance()
-	var spawn_position = rng.randi_range (1, 2)
-	add_child(spawn)
-	if spawn_position == 1:
-		spawn.position.x = 48
-	if spawn_position == 2:
-		spawn.position.x = -48
-	spawn.position.y = -32
+
